52f93c985c466d216b725dc552e3980f13d2b0f7
rodrigopk <rodrigopk@gmail.com>
2019-12-28 13:00:35 +0100

    Add endpoint to list Todos

--- controllers/todos.js
+++ controllers/todos.js
@@ -5,7 +5,12 @@ module.exports = {
     return Todo.create({
       title: req.body.title,
     })
-      .then(todo => res.status(201).send(todo))
+      .then(todo => res.status(200).send(todo))
+      .catch(error => res.status(400).send(error));
+  },
+  list(req, res) {
+    return Todo.findAll()
+      .then(todos => res.status(200).send(todos))
       .catch(error => res.status(400).send(error));
   },
 };
