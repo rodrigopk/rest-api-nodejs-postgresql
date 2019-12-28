f8610834d4b38ed8f03adcc6457c9eb7dd7ee40a
rodrigopk <rodrigopk@gmail.com>
2019-12-28 11:46:54 +0100

    Initial commit
    Initial npm configuration
    Added express dev dependency
    Added yarn configuration
    Added placeholder json response

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'REST API with Node.js, and Postgres API' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}.`);
+});
+
+module.exports = app;
